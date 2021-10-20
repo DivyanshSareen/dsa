61062efde4aab91a3f92339c993081e28fec0d49
DivyanshSareen <sareendivyansh@gmail.com>
2021-10-20 18:38:02 +0530

    Create mediansream.cc
    debug krte krte gfg ka solution copy ho gya pura, ...answer fr b glt ara h

---
+++ mediansream.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double arr[] = {5, 15, 10, 20, 3};
+
+    priority_queue<double> s;
+    priority_queue<double, vector<double>, greater<double>> g;
+
+    double med = arr[0];
+    s.push(arr[0]);
+
+    cout << med << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        double x = arr[i];
+        if (s.size() > g.size())
+        {
+            if (x < med)
+            {
+                g.push(s.top());
+                s.pop();
+                s.push(x);
+            }
+            else
+            {
+                g.push(x);
+            }
+            med = (g.top() + s.top()) / 2.0;
+        }
+        else if (s.size() == g.size())
+        {
+            if (x < med)
+            {
+                s.push(x);
+                med = (double)s.top();
+            }
+            else
+            {
+                g.push(x);
+                med = (double)g.top();
+            }
+        }
+        else
+        {
+            if (x > med)
+            {
+                s.push(g.top());
+                g.pop();
+                g.push(x);
+            }
+            else
+                s.push(x);
+            med = (g.top() + s.top()) / 2.0;
+        }
+        cout << med << endl;
+    }
+    return 0;
+}
