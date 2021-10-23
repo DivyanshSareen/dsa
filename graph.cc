246bcade793944664baccf268a269cb7c06e0dae
DivyanshSareen <sareendivyansh@gmail.com>
2021-10-23 16:45:11 +0530

    changed graph implementation
    graph now represented as map<int, list<int>>

--- graph.cc
+++ graph.cc
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void addEdge(vector<int> g[], int a, int b)
+void addEdge(map<int, list<int>> &g, int a, int b)
 {
     g[a].push_back(b);
     g[b].push_back(a);
 }
 
-void printGraph(vector<int> g[], int v)
+void printGraph(map<int, list<int>> g)
 {
-    for (int i = 0; i < v; i++)
+    for (auto x : g)
     {
-        cout << "vertex " << i << ": ";
-        for (auto x : g[i])
+        cout << "vertex " << x.first << ": ";
+        for (auto y : x.second)
         {
-            cout << x << " ";
+            cout << y << " ";
         }
         cout << endl;
     }
@@ -22,7 +22,7 @@ void printGraph(vector<int> g[], int v)
 
 int main()
 {
-    vector<int> g[5];
+    map<int, list<int>> g;
     addEdge(g, 0, 1);
     addEdge(g, 0, 4);
     addEdge(g, 1, 2);
@@ -31,5 +31,7 @@ int main()
     addEdge(g, 2, 3);
     addEdge(g, 3, 4);
 
-    printGraph(g, 5);
+    printGraph(g);
+
+    return 0;
 }
